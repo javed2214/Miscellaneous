68c45ae9268b628d39a9a288b2aaf82794f3e4cb
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-05-15 15:34:42 +0530

    Add files via upload

---
+++ MACROS.cpp
@@ -0,0 +1,47 @@
+
+/*---------------------------
+    Author : Javed Ansari
+    Date   : 17 April 2019    
+-----------------------------*/
+
+#include<bits/stdc++.h>
+#include<cstring>
+#include<stdio.h>
+using namespace std;
+#define MOD1 1000000007
+#define MOD2 1000000009
+#define DB(x) cout<<#x<<"="<<x<<"\n";
+#define FASTREAD ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define SORT(a) sort(a.begin(),a.end())
+#define test(t) ll t; cin>>t; while(t--)
+#define pi 2*acos(0.0)
+#define endl cout<<"\n";
+#define sss cout<<"*";
+#define REPLACE(str,start,count,new_str) str.replace(start,count,new_str)
+#define COUNT(str,ch) count(str.begin(),str.end(),ch)
+#define ERASE(str,i) str.erase(str.begin()+i)
+#define maxa(a,n) *max_element(a,a+n)
+#define mina(a,n) *min_element(a,a+n)
+#define rev(v) reverse(v.begin(),v.end())
+#define SUM(v) accumulate(v.begin(),v.end(),0)
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+#define ll long long int
+#define ld long double
+#define SIZE 100000001
+#define INF 0x7fffffff
+
+
+void solveQues(){
+
+	
+}
+
+
+int main(){
+
+	FASTREAD;
+	solveQues();
+
+	return 0;
+}
